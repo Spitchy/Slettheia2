1e254dd5b1ad9accb467db05fd5d1183ad7effd2
Spitchy <105996809+Spitchy@users.noreply.github.com>
2022-12-22 09:08:13 +0100

    endret api til apu retry

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app
     var Vipps_System_Plugin_Name = "vipps-postman";
     var Vipps_System_Plugin_Version = "2.0";
     var baseUrl = "https://slettheiaskole.vercel.app";
-    // var baseUrl = "http://localhost:3000";
+    //var baseUrl = "http://localhost:3000";
 
     // Get Token
     server.get("/api/getToken", (req, res) => {
